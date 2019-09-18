2ace6e76cc30a27f9567835cf878faf091cc229f
BensonLiao <skylock777@gmail.com>
2019-09-18 17:23:00 +0800

    Updates

---
+++ precache-manifest.9a8a9a9f9c632b3f0786c5d47730a919.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895e6d2eadd9d3431c61cd1bfcfb9f35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b1e3f30904a7c5bd752",
+    "url": "./static/js/2.b812a691.chunk.js"
+  },
+  {
+    "revision": "40a67a51af60092d10e5",
+    "url": "./static/js/3.d1640d77.chunk.js"
+  },
+  {
+    "revision": "f9610b887ccd9feace74",
+    "url": "./static/js/4.4a7c9660.chunk.js"
+  },
+  {
+    "revision": "6c56f5f636b25fb4f3df",
+    "url": "./static/js/5.d60be82d.chunk.js"
+  },
+  {
+    "revision": "fb792a399174b6930290",
+    "url": "./static/js/main.0c19efac.chunk.js"
+  },
+  {
+    "revision": "6040e3e2d1ca0093bd72",
+    "url": "./static/js/runtime~main.4ad9b4c2.js"
+  },
+  {
+    "revision": "c49fa063a986eea477d31d2e16d6f919",
+    "url": "./static/media/blue-check.c49fa063.png"
+  },
+  {
+    "revision": "3e14c772d9e4d949ad99a94b8a7c786c",
+    "url": "./static/media/comment-menu-2.3e14c772.png"
+  },
+  {
+    "revision": "6aed4496d796625c221b8b3d83ef0e13",
+    "url": "./static/media/english.6aed4496.png"
+  },
+  {
+    "revision": "3c65424dc193b0865a30f1710807b863",
+    "url": "./static/media/fb-emojis-new.3c65424d.png"
+  },
+  {
+    "revision": "36b90f96a12de5fd63a1822794afedad",
+    "url": "./static/media/global.36b90f96.png"
+  },
+  {
+    "revision": "06f0666e8ea60c4ca0487236ee8858bf",
+    "url": "./static/media/korean-fish.06f0666e.png"
+  },
+  {
+    "revision": "f320b87a42b8f14fa1c62cbd093d375d",
+    "url": "./static/media/land.f320b87a.png"
+  },
+  {
+    "revision": "1b5fa9821cd37c4350a2d817367b202f",
+    "url": "./static/media/mazu.1b5fa982.png"
+  },
+  {
+    "revision": "0d7c9f5ac84f863f193e7a7beca4549d",
+    "url": "./static/media/menu-new.0d7c9f5a.png"
+  },
+  {
+    "revision": "e2a06f400841330b4be59c386b792416",
+    "url": "./static/media/my-comment.e2a06f40.png"
+  },
+  {
+    "revision": "7b06d1bde5aa937c41260bc5363f8956",
+    "url": "./static/media/relevant.7b06d1bd.png"
+  },
+  {
+    "revision": "9c75973b91c578f4522eb8a55d363338",
+    "url": "./static/media/responseShare.9c75973b.png"
+  },
+  {
+    "revision": "13565405a3a34f860f312bfe584712cd",
+    "url": "./static/media/sparkJoy.13565405.jpg"
+  },
+  {
+    "revision": "7dd2ef4852049cf6351b690333bb8126",
+    "url": "./static/media/thumbUp.7dd2ef48.png"
+  },
+  {
+    "revision": "1549640740094a0695f0504aca8fbd71",
+    "url": "./static/media/wheel.15496407.png"
+  },
+  {
+    "revision": "ed46c93232c3175427a222a0fbb15aca",
+    "url": "./static/media/wvWFAMT.ed46c932.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a803250adbce4678e5f1ebefbe9dea02.js"
+  "./precache-manifest.9a8a9a9f9c632b3f0786c5d47730a919.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
